e3e559d26472e3c55c8f176af49fd77eaff2bc6a
Sergey Kazmin <kazmin5796@mail.ru>
2022-07-07 22:14:45 +0300

    Change main

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,26 +2,27 @@ package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
 
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
-        userDaoJDBC.createUsersTable();
-        userDaoJDBC.saveUser("Sergey", "Kazmin", (byte)26);
+        UserServiceImpl userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Sergey", "Kazmin", (byte)26);
         System.out.println("User с именем – Sergey добавлен в базу данных");
-        userDaoJDBC.saveUser("Julia", "Pavlyuk", (byte)19);
+        userService.saveUser("Julia", "Pavlyuk", (byte)19);
         System.out.println("User с именем – Julia добавлен в базу данных");
-        userDaoJDBC.saveUser("Vitaliy", "Sidorov", (byte)26);
+        userService.saveUser("Vitaliy", "Sidorov", (byte)26);
         System.out.println("User с именем – Vitaliy добавлен в базу данных");
-        userDaoJDBC.saveUser("Grigory", "Kazmin", (byte)20);
+        userService.saveUser("Grigory", "Kazmin", (byte)20);
         System.out.println("User с именем – Grigory добавлен в базу данных");
-        List<User> users =  userDaoJDBC.getAllUsers();
+        List<User> users =  userService.getAllUsers();
         for (var user : users) {
             System.out.println(user);
         }
-        userDaoJDBC.cleanUsersTable();
-        userDaoJDBC.dropUsersTable();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
