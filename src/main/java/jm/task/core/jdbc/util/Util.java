71335e71ca24dde5c4f590c9ada16a7ce9c6a3ee
Sergey Kazmin <kazmin5796@mail.ru>
2022-07-14 01:45:29 +0300

    Try 1

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,11 +1,18 @@
 package jm.task.core.jdbc.util;
 
 import com.mysql.cj.jdbc.Driver;
+import jm.task.core.jdbc.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.*;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.*;
 
 public class Util {
     // реализуйте настройку соеденения с БД
@@ -24,5 +31,27 @@ public class Util {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+    public static SessionFactory getSessionFactory() {
+        return null;
+    }
+    public static Session getCurrentSession() {
+        Map<String, String> settings = new HashMap<>();
+        settings.put("connection.driver_class", "com.mysql.jdbc.Driver");
+        settings.put("dialect", "org.hibernate.dialect.MySQL8Dialect");
+        settings.put("hibernate.connection.url", URL);
+        settings.put("hibernate.connection.username", USERNAME);
+        settings.put("hibernate.connection.password", PASSWORD);
+        settings.put("hibernate.current_session_context_class", "thread");
+
+        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                .applySettings(settings).build();
+
+        MetadataSources metadataSources = new MetadataSources(serviceRegistry);
+        metadataSources.addAnnotatedClass(User.class);
+        Metadata metadata = metadataSources.buildMetadata();
+        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
+        Session session = sessionFactory.getCurrentSession();
+        return session;
     }
 }
